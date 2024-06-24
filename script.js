65f735b52faa0e740c6f7753b555f62e82b22d85
xhide341 <shawnehgn10@gmail.com>
2024-06-24 15:38:20 +0800

    finished the project

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
+const resetButton = document.querySelector(".resetButton");
+const promptButton = document.querySelector(".promptButton");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.style.cssText = 'display: flex;';
+
+function createGrid(size) {
     
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement('div');
-        div.style.cssText = 'border: 1px solid black; width: 30px; height: 30px;';
-        
-        div.addEventListener('mouseenter', () => {
-            const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            div.style.backgroundColor = "#" + randomColor;
-        });
+    container.innerHTML = '';
 
-        div.addEventListener('mouseleave', () => {
+    const divSize = 700 / size;
+
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.style.cssText = 'display: flex; flex: 1;';
         
-        });
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement('div');
+            div.style.cssText = `border: 1px solid black; width: ${divSize}px; height: ${divSize}px; flex: 1;`;
+            
+            div.addEventListener('mouseenter', () => {
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                div.style.backgroundColor = "#" + randomColor;
+            });
+    
+            div.addEventListener('mouseleave', () => {
+            
+            });
+            
+            row.appendChild(div);
+        }
         
-        row.appendChild(div);
+        container.appendChild(row);
     }
-    
-    container.appendChild(row);
 }
 
-button.addEventListener('click', () => {
+
+function promptForGridSize() {
+    let size = prompt("Enter number of grids per side (max 100):");
+    size = parseInt(size);
+
+    if (isNaN(size) || size < 1) {
+        alert("Please enter a valid number.");
+        return promptForGridSize();
+    }
+
+    if (size > 100) {
+        alert("Maximum size is 100.");
+        return promptForGridSize();
+    }
+
+    createGrid(size);
+}
+
+resetButton.addEventListener('click', () => {
     const AllDivs = container.querySelectorAll('div > div');
     AllDivs.forEach(div => {
         div.style.backgroundColor = '';
     });
 });
+
+promptButton.addEventListener('click', () => {
+    promptForGridSize();
+});
+
+createGrid(50);
